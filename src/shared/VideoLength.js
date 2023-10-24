eaa62f385e4665a297b27e4fe3d715bb387f110a
Roshan <roshankumar40786@gmail.com>
2023-10-24 16:18:31 +0530

    fixed timping length

--- src/shared/VideoLength.js
+++ src/shared/VideoLength.js
@@ -2,10 +2,14 @@ import React from "react";
 import moment from "moment";
 
 const VideoLength = ({ time }) => {
-    const videoLengthInSeconds = moment()
-        ?.startOf("day")
-        ?.seconds(time)
-        ?.format("H:mm:ss");
+    let videoLengthInSeconds = moment()
+        .startOf("day")
+        .seconds(time)
+        .format("H:mm:ss");
+    if (videoLengthInSeconds.startsWith("0:")) {
+        videoLengthInSeconds = videoLengthInSeconds.substring(3);
+    }
+
     return (
         <span className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
             {videoLengthInSeconds}
